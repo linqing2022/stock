7818ffc5cd1df37d3236b5b6a1018ee11d70cd1d
csujklinqing <csujklinqing@163.com>
2022-11-22 07:03:29 +0800

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -1,3 +1,10 @@
 import pandas as pd
-df = pd.DataFrame({'A': [6, 87,  2], 'B': [26, 5, 30], 'C': [69, 89, 2]})
-print(df.corr(method='pearson')) # 默认是pearson，可以不写直接用df2.corr()
+code = '601636.SH'
+# dir_path = 'd:\\data\\tushare\\hfq_fina\\002466.SZ.csv'
+dir_path = 'd:\\data\\tushare\\hfq_fina\\' + code + '.csv'
+corr_path = 'd:\\data\\tushare\\corr\\' + code + '.csv'
+df = pd.read_csv(dir_path,error_bad_lines=False,low_memory=False) 
+df2 = df.corr(method='pearson')
+df2.to_csv(corr_path)
+#df2 = df.open.corr(df.ps_ttm,method='pearson')
+#print(df2)
